ea3d6e7b19ff1895f5f63eef1a73657338daacd0
jinsu4755 <parkjinsu4755@daum.net>
2020-04-15 15:47:59 +0900

    [CREATE] create View: add RegisterActivity

---
+++ app/src/main/java/kr/nutee/nutee_android/login/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package kr.nutee.nutee_android.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.nutee.nutee_android.R
+
+class RegisterActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_register)
+	}
+}
