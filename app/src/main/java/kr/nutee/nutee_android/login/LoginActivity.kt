4d6f51eb67d84983539005aac2b59945fd05abdb
jinsu4755 <parkjinsu4755@daum.net>
2020-04-15 15:47:19 +0900

    [UPDATE] login page: Add a transition to the register page

--- app/src/main/java/kr/nutee/nutee_android/login/LoginActivity.kt
+++ app/src/main/java/kr/nutee/nutee_android/login/LoginActivity.kt
@@ -17,20 +17,28 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_login)
 
+		text_forget_id_or_pw_button.setOnClickListener(this)
 		btn_login.setOnClickListener(this)
+		text_register_button.setOnClickListener(this)
 	}
 
 
 	//클릭 이벤트 구현
 	override fun onClick(v: View?) {
+		val logTag = "LoginActivityButtonEv"
 		//해당 View의 id 값을 가져올 변수 id 생성
 		val id = v!!.id
 
 		//각 버튼인 경우 동작 매칭
 		when (id) {
+			//id&pw 찾기 버튼 클릭시
+			R.id.text_forget_id_or_pw_button -> {
+				Log.v(logTag, "forget id or pw text button test")
+			}
+
 			//로그인 버튼 클릭시
 			R.id.btn_login -> {
-				Log.d("LoginButton:", "login button click")
+				Log.v(logTag, "login button click")
 
 				if (et_login_id.text.isNullOrBlank() || et_login_pw.text.isNullOrBlank()) {
 					when (true) {
@@ -41,6 +49,14 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
 					val intent = Intent(this, MainActivity::class.java)
 					startActivity(intent)
 				}
+			}
+
+			//회원가입 버튼 클릭시
+			R.id.text_register_button -> {
+				Log.v(logTag, "register button test")
+
+				val intent = Intent(this, RegisterActivity::class.java)
+				startActivity(intent)
 			}
 
 		}
