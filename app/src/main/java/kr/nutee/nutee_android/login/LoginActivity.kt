aca0a2cb216febdb224869408f79590f66f0306d
jinsu4755 <parkjinsu4755@daum.net>
2020-04-23 03:51:12 +0900

    [UPDATE] login page: Activate buttons when input id/pw

--- app/src/main/java/kr/nutee/nutee_android/login/LoginActivity.kt
+++ app/src/main/java/kr/nutee/nutee_android/login/LoginActivity.kt
@@ -3,8 +3,13 @@ package kr.nutee.nutee_android.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.login_activity.*
 import kr.nutee.nutee_android.R
@@ -12,9 +17,15 @@ import kr.nutee.nutee_android.main.MainActivity
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
+	private val REQUEST_CODE = 1
+
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.login_activity)
+
+		et_login_id.addTextChangedListener(textWatcher)
+		et_login_pw.addTextChangedListener(textWatcher)
 
 		text_forget_id_or_pw_button.setOnClickListener(this)
 		btn_login.setOnClickListener(this)
@@ -26,18 +37,17 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
 	override fun onClick(v: View?) {
 		val logTag = "LoginActivityButtonEv"
 		//해당 View의 id 값을 가져올 변수 id 생성
-		val id = v!!.id
 
 		//각 버튼인 경우 동작 매칭
-		when (id) {
+		when (v!!.id) {
 			//id&pw 찾기 버튼 클릭시
 			R.id.text_forget_id_or_pw_button -> {
-				Log.v(logTag, "forget id or pw text button test")
+				Log.d(logTag, "forget id or pw text button test")
 			}
 
 			//로그인 버튼 클릭시
 			R.id.btn_login -> {
-				Log.v(logTag, "login button click")
+				Log.d(logTag, "login button click")
 
 				if (et_login_id.text.isNullOrBlank() || et_login_pw.text.isNullOrBlank()) {
 					when (true) {
@@ -52,14 +62,30 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
 			//회원가입 버튼 클릭시
 			R.id.text_register_button -> {
-				Log.v(logTag, "register button test")
+				Log.d(logTag, "register button test")
 
 				val intent = Intent(this, RegisterActivity::class.java)
-				startActivity(intent)
+				startActivityForResult(intent,REQUEST_CODE)
 			}
 
 		}
 	}
 
+	private val textWatcher = object : TextWatcher {
+		override fun afterTextChanged(s: Editable?) {
+
+		}
+
+		override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+		}
+
+		override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+			btn_login.isEnabled = et_login_id.text.isNotEmpty() && et_login_pw.text.isNotEmpty()
+		}
+
+	}
+
 
 }
+
