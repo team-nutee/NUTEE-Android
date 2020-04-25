255c9cdbf26877277f3e51beca37969c8ea927ec
jinsu4755 <parkjinsu4755@daum.net>
2020-04-26 04:03:32 +0900

    [UPDATE] Modify screen transitions

--- app/src/main/java/kr/nutee/nutee_android/ui/main/MainActivity.kt
+++ app/src/main/java/kr/nutee/nutee_android/ui/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package kr.nutee.nutee_android.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -8,6 +9,7 @@ import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.main_activity.*
 import kr.nutee.nutee_android.R
+import kr.nutee.nutee_android.ui.main.fragment.add.AddActivity
 import kr.nutee.nutee_android.ui.main.fragment.home.HomeFlagement
 import kr.nutee.nutee_android.ui.main.fragment.notice.NoticeFlagment
 import kr.nutee.nutee_android.ui.main.fragment.profile.ProfileFragment
@@ -28,12 +30,12 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction().replace(R.id.frame_layout, HomeFlagement()).commitAllowingStateLoss()
 
         //navigationBottomView 등록
-        mainNavigationBottomView(main_bottom_nav, supportFragmentManager)
+        mainNavigationBottomView(main_bottom_nav)
     }
 
     // NavigationBottomView 화면전환
-    private fun mainNavigationBottomView(bottomNavigationView: BottomNavigationView, fragmentManager: FragmentManager){
-        main_bottom_nav.setOnNavigationItemSelectedListener {
+    private fun mainNavigationBottomView(bottomNavigationView: BottomNavigationView){
+        bottomNavigationView.setOnNavigationItemSelectedListener {
             when(it.itemId){
                 R.id.menu_home-> {
                     main_title.text = resources.getText(R.string.fragment_home)
@@ -48,7 +50,9 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 R.id.menu_add-> {
-                    loadFragment(HomeFlagement())
+                    val intent = Intent(this, AddActivity::class.java)
+                    startActivity(intent)
+
                     return@setOnNavigationItemSelectedListener true
                 }
 
