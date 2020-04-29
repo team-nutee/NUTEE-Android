06057b55db270082991ef6b3134bfdf844fb626c
jinsu4755 <parkjinsu4755@daum.net>
2020-04-30 03:12:39 +0900

    [CREATE] Create Notice Flagment and View

---
+++ app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/notice/classnotice/ClassFlagment.kt
@@ -0,0 +1,21 @@
+package kr.nutee.nutee_android.ui.main.fragment.notice.classnotice
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import kr.nutee.nutee_android.R
+
+
+class ClassFlagment : Fragment() {
+
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.notice_fragment_class, container, false)
+	}
+}
---
+++ app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/notice/eventnotice/EventFlagment.kt
@@ -0,0 +1,24 @@
+package kr.nutee.nutee_android.ui.main.fragment.notice.eventnotice
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import kr.nutee.nutee_android.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class EventFlagment : Fragment() {
+
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.notice_fragment_event, container, false)
+	}
+
+}
