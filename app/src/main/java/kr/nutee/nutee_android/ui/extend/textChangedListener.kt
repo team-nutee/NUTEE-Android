0f39d3f1b538bc062f3258d46af7d1dc479f1764
jinsu4755 <parkjinsu4755@gmail.com>
2020-05-21 11:55:39 +0900

    [CREATE] Create textChangedListener Extension Function

---
+++ app/src/main/java/kr/nutee/nutee_android/ui/extend/textChangedListener.kt
@@ -0,0 +1,18 @@
+package kr.nutee.nutee_android.ui.extend
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
+fun EditText.textChangedListener(textChanged : (CharSequence?) -> Unit) {
+	this.addTextChangedListener(object : TextWatcher {
+		override fun afterTextChanged(s: Editable?) {
+			textChanged(s)
+		}
+
+		override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+
+		override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+
+	})
+}
