4384828b329505271fa1d26d26ddb3705ee7d7a6
jinsu4755 <parkjinsu4755@gmail.com>
2020-05-25 03:21:16 +0900

    [UPDATE] customEnqueue fix

--- app/src/main/java/kr/nutee/nutee_android/ui/extend/customEnqueue.kt
+++ app/src/main/java/kr/nutee/nutee_android/ui/extend/customEnqueue.kt
@@ -6,20 +6,21 @@ import retrofit2.Callback
 import retrofit2.Response
 
 fun <ResponseType> Call<ResponseType>.customEnqueue(
-	onFail : () -> Unit = { Log.d("Network", "통신에 실패했습니다.")},
-	onSuccess: (ResponseType) -> Unit,
-	onError: () -> Unit
+	onFail: () -> Unit = { Log.d("Network", "통신에 실패했습니다.") },
+	onSuccess: (Response<ResponseType>) -> Unit
 )
 {
+	val networkLog = "Network"
 	this.enqueue(object : Callback<ResponseType> {
 		override fun onFailure(call: Call<ResponseType>, t: Throwable) {
+			Log.d(networkLog, t.toString())
+			Log.d(networkLog, call.request().toString())
 			onFail()
 		}
 
 		override fun onResponse(call: Call<ResponseType>, response: Response<ResponseType>) {
-			response.body()?.let{
-				onSuccess(it)
-			}
+			Log.d(networkLog, "통신에 성공했습니다.")
+			onSuccess(response)
 		}
 	})
 }
