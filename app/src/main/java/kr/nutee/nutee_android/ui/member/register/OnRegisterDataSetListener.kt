e7cea60ba775570b61c6f74dd00c2fd3a7b5548b
jinsu4755 <parkjinsu4755@daum.net>
2020-08-02 15:16:09 +0900

    [Feat] 회원가입 Fragment: interface 작성
    1.OnRegisterDataSetListener 작성
    
    현재 fragment에서 해당 interface를 불러오는 과정과 지워주는 과정만 작성함

---
+++ app/src/main/java/kr/nutee/nutee_android/ui/member/register/OnRegisterDataSetListener.kt
@@ -0,0 +1,6 @@
+package kr.nutee.nutee_android.ui.member.register
+
+interface OnRegisterDataSetListener {
+
+	fun onRegisterEmailDataSetListener(email:String):Unit
+}
--- app/src/main/java/kr/nutee/nutee_android/ui/member/register/fragment/EmailAuthFragment.kt
+++ app/src/main/java/kr/nutee/nutee_android/ui/member/register/fragment/EmailAuthFragment.kt
@@ -1,18 +1,36 @@
 package kr.nutee.nutee_android.ui.member.register.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kr.nutee.nutee_android.R
+import kr.nutee.nutee_android.ui.member.register.OnRegisterDataSetListener
+import java.lang.RuntimeException
 
 class EmailAuthFragment : Fragment() {
+
+	private var onRegisterDataSetListener:OnRegisterDataSetListener? = null
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
 		return inflater.inflate(R.layout.member_register_email_auth_fragment, container, false)
+	}
+
+	override fun onAttach(context: Context) {
+		super.onAttach(context)
+		if (context !is OnRegisterDataSetListener) {
+			throw RuntimeException(context.toString() + "must implement OnRegisterDataSetListener")
+		}
+		onRegisterDataSetListener = context
+	}
+
+	override fun onDetach() {
+		super.onDetach()
+		onRegisterDataSetListener = null
 	}
 }
