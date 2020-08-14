9a593c4b09df6f2c6326d9d7919e82e591ecfe76
eunseo5355 <55755686+eunseo5355@users.noreply.github.com>
2020-08-15 01:38:38 +0900

    [Feat] 알림 뷰 그리기
    1. 상단바와 RecyclerView 그리기

---
+++ app/src/main/java/kr/nutee/nutee_android/ui/notification/NotificationActivity.kt
@@ -0,0 +1,13 @@
+package kr.nutee.nutee_android.ui.notification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.nutee.nutee_android.R
+
+class NotificationActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_notification)
+	}
+}
