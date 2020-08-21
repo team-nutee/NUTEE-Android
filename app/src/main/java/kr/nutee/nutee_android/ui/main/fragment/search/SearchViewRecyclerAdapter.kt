dc4ea329b0becc3180a2d0996917e48cd0d445da
Lim YunHwi <88yhtserof@naver.com>
2020-08-22 02:24:12 +0900

    [Question] 검색 기능 : 이전 검색 기록 부분 삭제
    getSharedPreferences()가 Unresolved reference라고 떠서 remove(key)를 사용할 수 없다.
      따라서 왜 위의 코드가 안되는 지 궁금하고,
      더해서 SearchView 에서도 다른 방법으로 부분 삭제 기능을 추가하려고 했는데 잘 되지 않아
      다른 사람이라면 어떻게 부분 삭제 기능을 구현할 것인지 궁금하다.

--- app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/search/SearchViewRecyclerAdapter.kt
+++ app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/search/SearchViewRecyclerAdapter.kt
@@ -38,13 +38,25 @@ class SearchViewRecyclerAdapter(var previousSearchResultsList:ArrayList<String>)
 
 
 
-
-
 	//이전 검색어 부분 삭제
 	fun removeItemView(position: Int) {
 		previousSearchResultsList.removeAt(position)
 		notifyItemRemoved(position)
 		notifyItemRangeChanged(position, previousSearchResultsList.size)
 
+
+		/*
+		질문:
+		val sharedPreferences = getSharedPreferences("search", Context.MODE_PRIVATE)
+		val editor=sharedPreferences.edit()
+		editor.remove("search${position+1}")
+		editor.apply()
+
+		SharedPreferences 부분 삭제 기능을 추가하려고 위에 코드를 47번째 줄부터 넣고 싶은데,
+		getSharedPreferences()가 Unresolved reference라고 떠서 remove(key)를 사용할 수 없다.
+		따라서 왜 위의 코드가 안되는 지 궁금하고,
+		더해서 SearchView 에서도 다른 방법으로 부분 삭제 기능을 추가하려고 했는데 잘 되지 않아
+		다른 사람이라면 어떻게 부분 삭제 기능을 구현할 것인지 궁금하다.
+		*/
 	}
 }
