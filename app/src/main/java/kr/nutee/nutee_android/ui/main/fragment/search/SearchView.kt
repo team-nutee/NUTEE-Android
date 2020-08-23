0b84c8e706fa87c36e6194115af9e35050e43374
Lim YunHwi <88yhtserof@naver.com>
2020-08-24 03:47:31 +0900

    [Feat] 검색 뷰: 기능 작성
    1.  sharedpreference 싱글톤으로 수정
    2. 이전 검색 기록 부분 삭제 기능 수정
    
    close #58

--- app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/search/SearchView.kt
+++ app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/search/SearchView.kt
@@ -1,9 +1,7 @@
 package kr.nutee.nutee_android.ui.main.fragment.search
 
-import android.content.Context
-import android.content.SharedPreferences
+
 import android.os.Bundle
-import android.util.Log
 import android.view.KeyEvent
 import android.view.KeyEvent.ACTION_DOWN
 import android.view.View
@@ -12,8 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_search_view.*
 import kr.nutee.nutee_android.R
-
-import kotlin.collections.ArrayList
+import kr.nutee.nutee_android.data.App
 
 class SearchView : AppCompatActivity() {
 
@@ -22,28 +19,20 @@ class SearchView : AppCompatActivity() {
 	private lateinit var searchViewRecyclerAdapter:SearchViewRecyclerAdapter
 	//검색어 문자열 선언
 	lateinit var searchBoxText:String
-	// SharedPreferences 객체 선언
-	lateinit var sharedPreferences:SharedPreferences
 
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_search_view)
 
-		// SharedPreferences 객체 얻기
-		sharedPreferences = getSharedPreferences("search", Context.MODE_PRIVATE)
-		val editor = sharedPreferences.edit()
-
-
-		//sharedPreferences에 저장된 key 리스트
-		val test: List<String> =sharedPreferences.all.map { it.key }
-		Log.d("리스트 확인", test.toString())
+		//전역 SharedPreference
+		val prefsSearch=App.prefsSearch
 
 
 		//이전 검색어 기록, 리스트에 저장(검색한 순서대로 저장되도록 key 값 확인)
-		for(key in 1.. test.size){
-			if(sharedPreferences.contains("search$key")){
-				sharedPreferences.getString("search$key","")?.let { previousSearchResultsList.add(it) }
+		for(key in 1..prefsSearch.KeyList.size){
+			if(prefsSearch.contains(key)) {
+				previousSearchResultsList.add(prefsSearch.getString(key))
 			}
 		}
 		//최근 검색 기록이 상단에 뜨도록 역순으로 정렬
@@ -54,30 +43,25 @@ class SearchView : AppCompatActivity() {
 		//검색어 입력
 		et_search_box.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
 			if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == ACTION_DOWN) {
+				//검색어
+				searchBoxText=et_search_box.text.toString()
 
-					searchBoxText=et_search_box.text.toString()
+				//검색어가 없는 경우
+				if (searchBoxText.length == 0) {
+					Toast.makeText(this, "검색어를 입력해 주세요", Toast.LENGTH_LONG).show()
+				}
+				//검색어가 있는 경우
+				if (searchBoxText.length != 0) {
+					//SharedPreferences에 검색어 저장
+					prefsSearch.setString(prefsSearch.KeyList.size+1, searchBoxText)
 
-					//검색어가 없는 경우
-					if (searchBoxText.length == 0) {
-						Toast.makeText(this, "검색어를 입력해 주세요", Toast.LENGTH_LONG).show()
-					}
-					//검색어가 있는 경우
-					if (searchBoxText.length != 0) {
+					//저장된 이전 검색어 받기
+					val inputText=prefsSearch.getString(prefsSearch.KeyList.size+1)
 
-						//SharedPreferences에 검색어 저장
-						editor.putString("search${previousSearchResultsList.size+1}", searchBoxText)
-						editor.apply()
-
-						//저장된 이전 검색어 받기
-						val inputText=sharedPreferences.getString("search${previousSearchResultsList.size+1}","")
-
-						//데이터 배열 준비: 이전 검색어 리스트에 추가
-						previousSearchResultsList.add(inputText.toString())
-					}
-
-					//sharedPreferences 목록 확인을 위한 Log
-					Log.d("확인",sharedPreferences.all.toString())
-					return@OnKeyListener true
+					//데이터 배열 준비: 이전 검색어 리스트에 추가
+					previousSearchResultsList.add(inputText.toString())
+				}
+				return@OnKeyListener true
 			}
 			false
 		})
@@ -85,10 +69,9 @@ class SearchView : AppCompatActivity() {
 
 		//어댑터 인스턴스 생성
 		searchViewRecyclerAdapter=SearchViewRecyclerAdapter(previousSearchResultsList)
-
 		rv_search_previous_search_results.apply{
 			//RecyclerView 설정
-			layoutManager=LinearLayoutManager(this@SearchView, LinearLayoutManager.VERTICAL, false)
+			layoutManager=LinearLayoutManager(this@SearchView,LinearLayoutManager.VERTICAL,false)
 			adapter=searchViewRecyclerAdapter
 		}
 
@@ -105,11 +88,8 @@ class SearchView : AppCompatActivity() {
 			previousSearchResultsList.clear()
 			//어댑터에서도 적용
 			rv_search_previous_search_results.adapter?.notifyDataSetChanged()
-
-			editor.clear()
-			editor.commit()
+			//SharesPreferece 목록에서 전체 삭제
+			prefsSearch.AllDelete()
 		}
-
 	}
-
 }
--- app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/search/SearchViewRecyclerAdapter.kt
+++ app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/search/SearchViewRecyclerAdapter.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import kr.nutee.nutee_android.R
+import kr.nutee.nutee_android.data.App
 
 class SearchViewRecyclerAdapter(var previousSearchResultsList:ArrayList<String>)
 	:RecyclerView.Adapter<SearchViewHolder>(){
@@ -40,23 +41,22 @@ class SearchViewRecyclerAdapter(var previousSearchResultsList:ArrayList<String>)
 
 	//이전 검색어 부분 삭제
 	fun removeItemView(position: Int) {
+		//전역 SharedPreference
+		val prefsSearch=App.prefsSearch
+		Log.d("ture","position${position.toString()}")
+
+		//제거될 아이템 위치부터 다시 SharedPreference 저장
+		for(n in position downTo 0 ) {
+			if(position>0){
+				prefsSearch.setString(
+				prefsSearch.KeyList.size-n,
+				prefsSearch.getString(prefsSearch.KeyList.size-n+1))
+			}
+		}
+		prefsSearch.remove(prefsSearch.KeyList.size)
+
 		previousSearchResultsList.removeAt(position)
 		notifyItemRemoved(position)
 		notifyItemRangeChanged(position, previousSearchResultsList.size)
-
-
-		/*
-		질문:
-		val sharedPreferences = getSharedPreferences("search", Context.MODE_PRIVATE)
-		val editor=sharedPreferences.edit()
-		editor.remove("search${position+1}")
-		editor.apply()
-
-		SharedPreferences 부분 삭제 기능을 추가하려고 위에 코드를 47번째 줄부터 넣고 싶은데,
-		getSharedPreferences()가 Unresolved reference라고 떠서 remove(key)를 사용할 수 없다.
-		따라서 왜 위의 코드가 안되는 지 궁금하고,
-		더해서 SearchView 에서도 다른 방법으로 부분 삭제 기능을 추가하려고 했는데 잘 되지 않아
-		다른 사람이라면 어떻게 부분 삭제 기능을 구현할 것인지 궁금하다.
-		*/
 	}
 }
