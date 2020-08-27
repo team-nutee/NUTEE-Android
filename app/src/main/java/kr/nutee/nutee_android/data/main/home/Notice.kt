d5ebbf94e2938302c5c288b06e6f192ac60ccd01
eunseo5355 <55755686+eunseo5355@users.noreply.github.com>
2020-08-28 02:35:56 +0900

    [Feat] 공지사항: 서버 연결

--- app/src/main/java/kr/nutee/nutee_android/data/main/home/Notice.kt
+++ app/src/main/java/kr/nutee/nutee_android/data/main/home/Notice.kt
@@ -1,9 +1,4 @@
 package kr.nutee.nutee_android.data.main.home
 
-data class Notice(
-	val no: String,
-	val title: String,
-	val href: String,
-	val author: String,
-	val date: String
-)
+data class NoticeList (val noticelist: List<Notice>)
+
---
+++ app/src/main/java/kr/nutee/nutee_android/data/main/home/NoticeItem.kt
@@ -0,0 +1,9 @@
+package kr.nutee.nutee_android.data.main.home
+
+data class Notice(
+	val no: String,
+	val title: String,
+	val href: String,
+	val author: String,
+	val date: String
+)
