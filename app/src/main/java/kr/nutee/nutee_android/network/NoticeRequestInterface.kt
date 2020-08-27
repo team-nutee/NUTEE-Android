3892588a6f67a589d0c673066b4deeb69e9a485a
eunseo5355 <55755686+eunseo5355@users.noreply.github.com>
2020-08-28 04:03:33 +0900

    [Feat] 공지사항: 서버 연결

--- app/src/main/java/kr/nutee/nutee_android/network/NoticeRequestInterface.kt
+++ app/src/main/java/kr/nutee/nutee_android/network/NoticeRequestInterface.kt
@@ -1,28 +1,42 @@
 package kr.nutee.nutee_android.network
 
+import androidx.media.AudioAttributesCompat
 import kr.nutee.nutee_android.data.main.home.Notice
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 
 interface NoticeRequestInterface {
 
 	@GET("haksa")
-	fun requestBachelor() : Call<Notice>
+	fun requestBachelor(
+		@Header("Contnet-Type") contentType: String = "application/x-www-form-urlencoded"
+	) : Call<Notice>
 
 	@GET("sooup")
-	fun requestClass() : Call<Notice>
+	fun requestClass(
+		@Header("Contnet-Type") contentType: String = "application/x-www-form-urlencoded"
+	) : Call<Notice>
 
 	@GET("hakjum")
-	fun requestExchange() : Call<Notice>
+	fun requestExchange(
+		@Header("Contnet-Type") contentType: String = "application/x-www-form-urlencoded"
+	) : Call<Notice>
 
 	@GET("janghak")
-	fun requestScholarship() : Call<Notice>
+	fun requestScholarship(
+		@Header("Contnet-Type") contentType: String = "application/x-www-form-urlencoded"
+	) : Call<Notice>
 
 	@GET("ilban")
-	fun requestGeneral() : Call<Notice>
+	fun requestGeneral(
+		@Header("Contnet-Type") contentType: String = "application/x-www-form-urlencoded"
+	) : Call<Notice>
 
 	@GET("hangsa")
-	fun requestEvent() : Call<Notice>
+	fun requestEvent(
+		@Header("Contnet-Type") contentType: String = "application/x-www-form-urlencoded"
+	) : Call<Notice>
 
 }
