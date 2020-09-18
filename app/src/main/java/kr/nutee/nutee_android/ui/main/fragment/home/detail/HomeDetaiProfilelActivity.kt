bf96f9791867a8e5a3f057f5dff3afd4aef4dc52
eunseo5355 <eunseo5355@naver.com>
2020-09-18 15:41:00 +0900

    [Feat] 유저 프로필에서 뒤로가기 버튼이랑 더보기 버튼 이벤트 리스너 작성

--- app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/home/detail/HomeDetaiProfilelActivity.kt
+++ app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/home/detail/HomeDetaiProfilelActivity.kt
@@ -2,15 +2,23 @@ package kr.nutee.nutee_android.ui.main.fragment.home.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.main_home_profile_detail_activity.*
 import kr.nutee.nutee_android.R
 import kr.nutee.nutee_android.data.App
+import kr.nutee.nutee_android.data.main.RequestReport
 import kr.nutee.nutee_android.data.main.home.ResponseMain
+import kr.nutee.nutee_android.data.main.home.ResponseMainItem
 import kr.nutee.nutee_android.data.main.profile.ResponseProfile
 import kr.nutee.nutee_android.network.RequestToServer
 import kr.nutee.nutee_android.ui.extend.customEnqueue
+import kr.nutee.nutee_android.ui.extend.dialog.cumstomReportDialog
+import kr.nutee.nutee_android.ui.extend.dialog.customSelectDialog
 import kr.nutee.nutee_android.ui.extend.imageSetting.setImageURLSetting
+import kr.nutee.nutee_android.ui.main.fragment.home.HomeFragement
 
 class HomeDetaiProfilelActivity : AppCompatActivity() {
 
@@ -27,8 +35,20 @@ class HomeDetaiProfilelActivity : AppCompatActivity() {
 		init()
 	}
 
+	override fun onBackPressed() {
+		super.onBackPressed()
+	}
+
 	private fun init(){
 		userID = intent.getIntExtra("Detail_Profile_id",0)
+
+		img_profile_detail_back_btn.setOnClickListener {
+			onBackPressed()
+		}
+
+		img_profile_detail_more.setOnClickListener {
+
+		}
 	}
 
 	private fun setAdapter(profilePostItem: ResponseMain) {
@@ -42,6 +62,25 @@ class HomeDetaiProfilelActivity : AppCompatActivity() {
 		Glide.with(applicationContext).load(userImageLoad).into(img_profile)
 		tv_profile_detail_content_num.text = res.Posts?.size.toString()
 		App.prefs.url = res.Image?.src
+	}
+
+	private fun moreEvent(it: View, customData: ResponseMainItem) {
+		img_profile_detail_more.context.customSelectDialog(
+			View.VISIBLE, View.GONE, View.GONE,
+			{
+				Log.d("글신고", "누름")
+				it.context.cumstomReportDialog{
+					requestToServer.service.requestReport(
+						RequestReport(it), customData.id)
+						.customEnqueue{ res->
+							if (res.isSuccessful) {
+								Toast
+									.makeText(img_profile_detail_more.context,"신고가 성공적으로 접수되었습니다.", Toast.LENGTH_SHORT)
+									.show()
+							}
+						}
+				}
+			})
 	}
 
 
