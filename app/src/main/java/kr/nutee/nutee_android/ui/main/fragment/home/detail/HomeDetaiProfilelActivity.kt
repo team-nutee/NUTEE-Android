4e33a1ab057d2efaf0bb86247b97a7deb0956bb7
eunseo5355 <eunseo5355@naver.com>
2020-10-05 02:55:22 +0900

    [Feat] 유저 프로필 뷰: 더보기 버튼 서버 연결 수정

--- app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/home/detail/HomeDetaiProfilelActivity.kt
+++ app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/home/detail/HomeDetaiProfilelActivity.kt
@@ -56,7 +56,7 @@ class HomeDetaiProfilelActivity : AppCompatActivity() {
 		requestToServer.service
 			.requestUserProfile(id)
 			.customEnqueue{response ->
-				response.body()?.let { bindUserProfile(it) } }
+				response.body()?.let { bindUserProfile(id, it) } }
 	}
 
 	private fun loadUserPost(id: Int, loadfun:(resMain:ResponseMain)->Unit) {
@@ -74,7 +74,7 @@ class HomeDetaiProfilelActivity : AppCompatActivity() {
 		rv_detail_profile.adapter = homeDetailProfileAdapter
 	}
 
-	private fun bindUserProfile(res: ResponseProfile) {
+	private fun bindUserProfile(userID: Int, res: ResponseProfile) {
 		tv_profile_detail_nickname.text = res.nickname
 		val userImageLoad = setImageURLSetting(res.Image?.src)
 		Glide.with(applicationContext).load(userImageLoad).into(img_profile)
@@ -82,27 +82,46 @@ class HomeDetaiProfilelActivity : AppCompatActivity() {
 		App.prefs.url = res.Image?.src
 
 		img_profile_detail_more.setOnClickListener {
-			profileMore(res)
+			profileMore(userID, res)
 		}
 	}
 
-	private fun profileMore(customData: ResponseProfile) {
-		customSelectDialog(
-			View.VISIBLE, View.GONE, View.GONE,
-			{
-				Log.d("글신고", "누름")
-				cumstomReportDialog{
-					requestToServer.service.requestReport(
-						RequestReport(it), customData.id)
-						.customEnqueue{ res->
-							if (res.isSuccessful) {
-								Toast
-									.makeText(img_profile_detail_more.context,"신고가 성공적으로 접수되었습니다.", Toast.LENGTH_SHORT)
-									.show()
-							}
+	private fun profileMore(userID: Int, customData: ResponseProfile) {
+		if (userID.toString() == App.prefs.local_user_id) {
+			customSelectDialog(View.GONE, View.VISIBLE, View.VISIBLE,
+				{},
+				{
+					Log.d("글수정 버튼", "누름")
+				},
+				{
+					Log.d("글삭제 버튼", "누름")
+					RequestToServer.service.requestDelete(
+						App.prefs.local_login_token,
+						customData.id
+					).customEnqueue {
+						if (it.isSuccessful) {
+							finish()
 						}
+					}
+				})
+		} else {
+			customSelectDialog(View.VISIBLE, View.GONE, View.GONE,
+				{
+					Log.d("글신고", "누름")
+					cumstomReportDialog{
+						RequestToServer.service.requestReport(
+							RequestReport(it), customData.id)
+							.customEnqueue{ res->
+								if (res.isSuccessful) {
+									Toast
+										.makeText(applicationContext,"신고가 성공적으로 접수되었습니다.", Toast.LENGTH_SHORT)
+										.show()
+								}
+							}
+					}
 				}
-			})
+			)
+		}
 	}
 
 
