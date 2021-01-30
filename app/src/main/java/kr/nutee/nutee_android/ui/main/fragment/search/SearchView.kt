260a7fe724d763f07537e9cca267aaced239e04a
Lim YunHwi <88yhtserof@naver.com>
2021-01-30 16:08:44 +0900

    [Refactor] 검색 기능 수정
    1. 검색 액티비티 주석 정리
    
    +이전 검색 기록 추후 수정 예정
    
    close #93

--- app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/search/SearchView.kt
+++ app/src/main/java/kr/nutee/nutee_android/ui/main/fragment/search/SearchView.kt
@@ -17,10 +17,10 @@ import kr.nutee.nutee_android.data.App
  * Created by 88yhtesrof
  * DESC: 검색 창 Activity 2.0 버전
  */
+//FIXME 이전 검색어 기록 기능 추후에 수정 예정
 
 class SearchView : AppCompatActivity() {
 
-	//데이터 배열 선언
 	private var previousSearchResultsList = ArrayList<String>()
 	private lateinit var searchViewRecyclerAdapter: SearchViewRecyclerAdapter
 
@@ -29,7 +29,7 @@ class SearchView : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_search_view)
 
-		//전역 SharedPreference
+		//SharedPreference
 		val prefsSearch = App.prefsSearch
 
 
@@ -46,14 +46,11 @@ class SearchView : AppCompatActivity() {
 		//검색어 입력
 		et_search_box.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
 			if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == ACTION_DOWN) {
-				//검색어
 				val searchBoxText = et_search_box.text.toString()
 
-				//검색어가 없는 경우
 				if (searchBoxText.length == 0) {
 					Toast.makeText(this, "검색어를 입력해 주세요", Toast.LENGTH_LONG).show()
 				}
-				//검색어가 있는 경우
 				if (searchBoxText.length != 0) {
 					//SharedPreferences에 검색어 저장
 					prefsSearch.setString(prefsSearch.KeyList.size + 1, searchBoxText)
@@ -61,7 +58,6 @@ class SearchView : AppCompatActivity() {
 					//저장된 이전 검색어 받기
 					val inputText = prefsSearch.getString(prefsSearch.KeyList.size + 1)
 
-					//데이터 배열 준비: 이전 검색어 리스트에 추가
 					previousSearchResultsList.add(inputText)
 
 					val intentSearchResults=Intent(this@SearchView,SearchResultsView::class.java)
@@ -72,32 +68,21 @@ class SearchView : AppCompatActivity() {
 			false
 		})
 
-
-		//어댑터 인스턴스 생성
 		searchViewRecyclerAdapter = SearchViewRecyclerAdapter(previousSearchResultsList)
 		rv_search_previous_search_results.apply {
-			//RecyclerView 설정
 			layoutManager =
 				LinearLayoutManager(this@SearchView, LinearLayoutManager.VERTICAL, false)
 			adapter = searchViewRecyclerAdapter
 		}
 
-
-		//'<' 뒤로 가기 버튼 기능
 		img_search_back_btn.setOnClickListener {
 			finish()
 		}
 
-		//'전체 삭제' 기능
 		tv_search_all_history_delete.setOnClickListener {
-			//이전 검색 결과 리스트 전체 삭제
 			previousSearchResultsList.clear()
-			//어댑터에서도 적용
 			rv_search_previous_search_results.adapter?.notifyDataSetChanged()
-			//SharesPreferece 목록에서 전체 삭제
 			prefsSearch.AllDelete()
 		}
-
 	}
-
 }
