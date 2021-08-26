9720ef8492ada1771d0ee42196e5107326bcc5fd
kimheeseon <gmltjs418@naver.com>
2021-08-27 01:13:59 +0900

    [Chore] 뷰 결합 방법 변경
    1. ui 내 setting 내 fragment 내 MainSettingFragment 뷰 결합 방식(viewBinding)으로 수정

--- app/src/main/java/kr/nutee/nutee_android/ui/setting/fragment/MainSettingFragment.kt
+++ app/src/main/java/kr/nutee/nutee_android/ui/setting/fragment/MainSettingFragment.kt
@@ -7,9 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.setting_activity.*
 import kotlinx.android.synthetic.main.setting_main_fragment.*
 import kr.nutee.nutee_android.R
 import kr.nutee.nutee_android.data.App
+import kr.nutee.nutee_android.databinding.MemberRegisterSelectDepartmentFragmentBinding
+import kr.nutee.nutee_android.databinding.SettingActivityBinding
+import kr.nutee.nutee_android.databinding.SettingMainFragmentBinding
 import kr.nutee.nutee_android.network.RequestToServer
 import kr.nutee.nutee_android.ui.extend.customEnqueue
 import kr.nutee.nutee_android.ui.extend.loadFragmentAddtoBackStack
@@ -27,70 +31,107 @@ import kr.nutee.nutee_android.ui.setting.SettingActivity
 
 class MainSettingFragment : Fragment() {
 
+	private var binding: SettingMainFragmentBinding?= null
+
 	override fun onCreateView(
-		inflater: LayoutInflater,
-		container: ViewGroup?,
-		savedInstanceState: Bundle?
+			inflater: LayoutInflater,
+			container: ViewGroup?,
+			savedInstanceState: Bundle?
 	): View? {
-		return inflater.inflate(R.layout.setting_main_fragment,container,false)
+		binding = SettingMainFragmentBinding.inflate(inflater, container, false)
+		return requireBinding().root
+		//return inflater.inflate(R.layout.setting_main_fragment,container,false)
 	}
+
+	private fun requireBinding(): SettingMainFragmentBinding = binding
+			?: error("binding is not init")
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
+		val binding by lazy { SettingActivityBinding.inflate(layoutInflater) }
+
 		//프로필 이미지 설정
-		tv_setting_profile_image_btn.setOnClickListener {
+		requireBinding().tvSettingProfileImageBtn.setOnClickListener {
 			requireContext().loadFragmentAddtoBackStack(ProfileImageSettingFragment()
-				,R.id.frame_layout_setting,null)
+					,binding.frameLayoutSetting.id,null)
 		}
+		/*tv_setting_profile_image_btn.setOnClickListener {
+			requireContext().loadFragmentAddtoBackStack(ProfileImageSettingFragment()
+					,R.id.frame_layout_setting,null)
+		}*/
+
 		//닉네임 설정
-		tv_setting_nickname_btn.setOnClickListener {
+		requireBinding().tvSettingNicknameBtn.setOnClickListener {
 			requireContext().loadFragmentAddtoBackStack(NickNameSettingFragment()
-				,R.id.frame_layout_setting,null)
+					,binding.frameLayoutSetting.id,null)
 		}
+		/*tv_setting_nickname_btn.setOnClickListener {
+			requireContext().loadFragmentAddtoBackStack(NickNameSettingFragment()
+					,R.id.frame_layout_setting,null)
+		}*/
+
 		//비밀번호 재설정
-		tv_setting_password_btn.setOnClickListener {
+		requireBinding().tvSettingPasswordBtn.setOnClickListener {
 			requireContext().loadFragmentAddtoBackStack(PasswordSettingFragment()
-				,R.id.frame_layout_setting,null)
+					,binding.frameLayoutSetting.id,null)
 		}
+		/*tv_setting_password_btn.setOnClickListener {
+			requireContext().loadFragmentAddtoBackStack(PasswordSettingFragment()
+					,R.id.frame_layout_setting,null)
+		}*/
+
 		//카테고리 재설정
-		tv_setting_category_btn.setOnClickListener {
+		requireBinding().tvSettingCategoryBtn.setOnClickListener {
 			requireContext().loadFragmentAddtoBackStack(CategorySettingFragment()
-				,R.id.frame_layout_setting,null)
+					,binding.frameLayoutSetting.id,null)
 		}
+		/*tv_setting_category_btn.setOnClickListener {
+			requireContext().loadFragmentAddtoBackStack(CategorySettingFragment()
+					,R.id.frame_layout_setting,null)
+		}*/
+
 		//전공 재설정
-		tv_setting_major_btn.setOnClickListener {
+		requireBinding().tvSettingMajorBtn.setOnClickListener {
 			requireContext().loadFragmentAddtoBackStack(MajorSettingFragment()
-				,R.id.frame_layout_setting,null)
+					,binding.frameLayoutSetting.id,null)
 		}
-		//개발자 정보
-		tv_setting_developer_information_btn.setOnClickListener {
-			val intent = Intent(context, DeveloperInformationActivity::class.java)
-			startActivity(intent)
-		}
-		//로그아웃
-		tv_setting_logout.setOnClickListener {
-			requestlogout()
+		/*tv_setting_major_btn.setOnClickListener {
+			requireContext().loadFragmentAddtoBackStack(MajorSettingFragment()
+					,R.id.frame_layout_setting,null)
+		}*/
+
+		with(requireBinding()){
+			//개발자 정보
+			tvSettingDeveloperInformationBtn.setOnClickListener {
+				val intent = Intent(context, DeveloperInformationActivity::class.java)
+				startActivity(intent)
+			}
+
+			//로그아웃
+			tvSettingLogout.setOnClickListener {
+				requestlogout()
+			}
 		}
 
 	}
 
 	private fun requestlogout(){
 		RequestToServer.authService
-			.reqeustLogout(App.prefs.local_login_token)
-			.customEnqueue(
-				onSuccess = {
-					App.prefs.local_login_id = ""
-					App.prefs.local_login_pw = ""
-					App.prefs.local_login_token = ""
-					App.prefs.local_login_oto=false
+				.reqeustLogout(App.prefs.local_login_token)
+				.customEnqueue(
+						onSuccess = {
+							App.prefs.local_login_id = ""
+							App.prefs.local_login_pw = ""
+							App.prefs.local_login_token = ""
+							App.prefs.local_login_oto=false
 
-					Toast.makeText(view?.context,it.body()!!.body, Toast.LENGTH_LONG).show()
-					val intent = Intent(view?.context, LoginActivity::class.java)
-					startActivity(intent)
-					activity?.finish()
-				}
-			)
+							Toast.makeText(view?.context,it.body()!!.body, Toast.LENGTH_LONG).show()
+							val intent = Intent(view?.context, LoginActivity::class.java)
+							startActivity(intent)
+							activity?.finish()
+						}
+				)
 	}
 
 }
