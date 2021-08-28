32aefa126b34b23efea9ba479f255b2bb074886f
kimheeseon <gmltjs418@naver.com>
2021-08-29 04:44:08 +0900

    [Chore] 뷰 결합 방법
    1. 뷰 결합 방식(viewBining)으로 변경하는 것을 시도했으나 잘 되지 않아 주석처리 해 놓은 상태. 추후 수정 필요(mian_title 및 오른쪽 상단 imageButton 변경 안됨)

--- app/src/main/java/kr/nutee/nutee_android/ui/extend/loadFragment.kt
+++ app/src/main/java/kr/nutee/nutee_android/ui/extend/loadFragment.kt
@@ -1,10 +1,32 @@
 package kr.nutee.nutee_android.ui.extend
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
+import android.os.Bundle
+import android.text.Layout
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.MenuInflater
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import com.google.android.material.internal.NavigationMenuItemView
+import kotlinx.android.synthetic.main.custom_loading_dialog.*
 import kotlinx.android.synthetic.main.main_activity.*
+import kotlinx.android.synthetic.main.main_activity.view.*
+import kotlinx.android.synthetic.main.term_of_use_activity.view.*
 import kr.nutee.nutee_android.R
+import kr.nutee.nutee_android.databinding.CustomSelectDialogBinding
+import kr.nutee.nutee_android.databinding.MainActivityBinding
+import kr.nutee.nutee_android.databinding.SearchActivityBinding
+import kr.nutee.nutee_android.databinding.SettingActivityBinding
+import kr.nutee.nutee_android.ui.extend.dialog.customSelectDialog
+import kr.nutee.nutee_android.ui.main.MainActivity
+
 
 /*
 * Created by jinsu4755
@@ -14,7 +36,15 @@ import kr.nutee.nutee_android.R
 //fragment load 함수
 fun Context.loadMainPageFragment(title: CharSequence, fragment: Fragment, searchView: Int, settingView: Int) {
 	val transaction =
-		(this as FragmentActivity).supportFragmentManager.beginTransaction()
+			(this as FragmentActivity).supportFragmentManager.beginTransaction()
+
+	/*val binding by lazy {MainActivityBinding.inflate(layoutInflater)}
+	binding.mainTitle.text = title
+	transaction.replace(binding.frameLayout.id, fragment)
+	transaction.commit()
+	binding.imgMainTopSearch.visibility = searchView
+	binding.imgMainTopSetting.visibility = settingView*/
+
 	main_title.text= title
 	transaction.replace(R.id.frame_layout, fragment)
 	transaction.commit()
@@ -23,8 +53,8 @@ fun Context.loadMainPageFragment(title: CharSequence, fragment: Fragment, search
 }
 
 fun Context.loadFragment(
-	fragment: Fragment,
-	view: Int
+		fragment: Fragment,
+		view: Int
 ){
 	val transaction
 			= (this as FragmentActivity).supportFragmentManager.beginTransaction()
@@ -33,14 +63,13 @@ fun Context.loadFragment(
 }
 
 fun Context.loadFragmentAddtoBackStack(
-	fragment: Fragment,
-	view:Int,
-	backStackName: String?
+		fragment: Fragment,
+		view: Int,
+		backStackName: String?
 ) {
 	val transaction =
-		(this as FragmentActivity).supportFragmentManager.beginTransaction()
+			(this as FragmentActivity).supportFragmentManager.beginTransaction()
 	transaction.replace(view, fragment)
 	transaction.addToBackStack(backStackName)
 	transaction.commit()
 }
-
